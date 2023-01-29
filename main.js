d29667159c7eb359ccd037c079340cceec72fc66
Lawll.n <92900098+nthi2003@users.noreply.github.com>
2023-01-29 22:13:51 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.nabar');
+const navLink = document.querySelector('.nav__link');
+
+nabar.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
